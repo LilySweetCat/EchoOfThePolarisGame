55bdc6858f986570cbfc2252651e3c3d4efdb2a2
Баева Лилия Алексеевна <lilia.baeva@norbit.ru>
2025-07-11 21:11:50 +0300

    clues

--- Scripts/clue_detection.gd
+++ Scripts/clue_detection.gd
@@ -3,11 +3,24 @@ extends RayCast3D
 @export var progress_speed: float = 0.1
 @export var progress: float = 0.0
 
-var _in_dialogue: bool
+var _disable_scan: bool
 var _cursor_material: ShaderMaterial
+var _timer: Timer
 
 func _ready() -> void:
 	_cursor_material = GameUi.cursor.material
+	
+	_timer = Timer.new()
+	_timer.wait_time = 1
+	_timer.one_shot = true
+	_timer.timeout.connect(on_timeout)
+	self.add_child(_timer)
+
+func on_timeout() -> void:
+	_cursor_material.set_shader_parameter("fill_amount", 0)
+	GameUi.toggle_cursor()
+	progress = 0
+	_disable_scan = false
 
 func _physics_process(delta: float) -> void:
 	if not is_colliding():
@@ -24,8 +37,8 @@ func _physics_process(delta: float) -> void:
 	progress = clamp(progress + (progress_speed * delta), 0, 1.0)
 	_cursor_material.set_shader_parameter("fill_amount", progress)
 	
-	if progress >= 1 and !_in_dialogue:
-		_in_dialogue = true
+	if progress >= 1 and !_disable_scan:
+		_disable_scan = true
 		GameUi.toggle_cursor()
 		GameUi.dialogue_ended.connect(on_dialogue_ended)
 		GameUi.play_dialogue(collider.dialogue.data)
@@ -33,9 +46,5 @@ func _physics_process(delta: float) -> void:
 
 func on_dialogue_ended() -> void:
 	GameUi.dialogue_ended.disconnect(on_dialogue_ended)
-	
-	_cursor_material.set_shader_parameter("fill_amount", 0)
-	GameUi.toggle_cursor()
-	progress = 0
-	_in_dialogue = false
+	_timer.start(1)
 	return
